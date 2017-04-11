36e0deae6e661f64bf748ec6d28428771f851c5d
Eun-Joo <eunjooyoon@hotmail.co.uk>
2017-04-11 15:12:15 +0100

    created basic files

---
+++ Photographer.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Photographer {
+
+  private ArrayList<Camera> collection;
+
+  public Photographer() {
+    this.collection = new ArrayList<Camera>();
+  }
+
+
+
+}
